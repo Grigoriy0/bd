5e86f906f9a5fae20b511236e97666d2a684e306
Grigory0 <grisha.konoplich0@gmail.com>
2021-11-06 16:20:17 +0300

    Init: Lab 3

---
+++ README.md
@@ -0,0 +1,10 @@
+# Databases
+
+Copy file `.env.tpl` to `.env` file and pass required variables
+```
+cp .env.tpl .env
+```
+and run docker-compose:
+```
+docker-compose up --build -d
+```
---
+++ music-shop.sql
@@ -0,0 +1,115 @@
+DROP DATABASE IF EXISTS shops;
+CREATE DATABASE shops;
+
+USE shops;
+
+CREATE TABLE `music-shop` (
+  UNP CHAR (9) PRIMARY KEY,
+  address VARCHAR (50)
+);
+CREATE TABLE `employees` (
+  passport CHAR(9) PRIMARY KEY,
+  position VARCHAR(30),
+  fullName VARCHAR(50)
+);
+CREATE TABLE `supplies` (
+  supplyId INTEGER PRIMARY KEY AUTO_INCREMENT,
+  supplier VARCHAR(50),
+  date DATE
+);
+CREATE TABLE `clients` (
+  phoneNumber CHAR(12) PRIMARY KEY,
+  firstName VARCHAR(15)
+);
+CREATE TABLE `products` (
+  productId INTEGER PRIMARY KEY AUTO_INCREMENT,
+  supplyId INTEGER NOT NULL,
+  price FLOAT(7, 2),
+  name VARCHAR (50),
+  description TEXT (256),
+  discount FLOAT(7, 2),
+  FOREIGN KEY (supplyId) REFERENCES supplies(supplyId)
+);
+CREATE TABLE `operations` (
+  operationId INTEGER (16) PRIMARY KEY AUTO_INCREMENT,
+  productId INTEGER NOT NULL,
+  employeePassport CHAR(9) NOT NULL,
+  clientPhone CHAR(12) NOT NULL,
+  operationDate DATETIME,
+  sum FLOAT(7, 2),
+  FOREIGN KEY (productId) REFERENCES products(productId),
+  FOREIGN KEY (employeePassport) REFERENCES employees(passport),
+  FOREIGN KEY (clientPhone) REFERENCES clients(phoneNumber)
+);
+
+INSERT INTO
+  `music-shop` (UNP, address)
+VALUES
+  ('S9DRG23C3', 'Nezavisimosty 55'),
+  ('HSXTMJ52B', 'Logoysky trakt 15'),
+  ('QB8TU2C7X', 'Nezavisimosty 93'),
+  ('3A78UQRPA', 'Nezavisimosty 58'),
+  ('DMQXC9Q45', 'Logoysky trakt 22');
+SELECT
+  *
+FROM
+  `music-shop`;
+
+INSERT INTO 
+  `employees` (passport, position, fullName)
+VALUES
+  ('HB3000000', 'Owner', 'Grigory Konoplich'),
+  ('HB3000001', 'Clean master', 'Alexey Grigarovich');
+SELECT
+  *
+FROM
+  `employees`;
+
+INSERT INTO
+  `clients` (phoneNumber, firstName)
+VALUES
+  ('375123456789', 'Grigory'),
+  ('375123456780', 'Vladick');
+SELECT
+  *
+FROM
+  `clients`;
+
+INSERT INTO
+  `supplies` (supplier, date)
+VALUES
+  ('Wooden Musical Instrument Model Gift', CURDATE()),
+  ('Allen Organ Company', CURDATE()),
+  ('Premier Promotions USA', CURDATE()),
+  ('Allen Organ Company', CURDATE());
+SELECT
+  *
+FROM
+  `supplies`;
+INSERT INTO
+  `products` (productId, supplyId, price, name, description, discount)
+VALUES
+  (1, 1, 127.80, "acoustic guitar", "The best mahogany guitar", 0.00),
+  (2, 2, 3.45, "mediator", "Mediator 0.8 mm", 0.00);
+SELECT
+  *
+FROM
+  `products`;
+
+INSERT INTO 
+  `operations` (operationDate, sum, productId, employeePassport, clientPhone)
+VALUES
+  ('2021-09-24 12:34:02', 127.80, 1, 'HB3000000', '375123456789'),
+  ('2021-09-24 12:34:02', 3.45, 2, 'HB3000001', '375123456780'),
+  ('2021-09-24 12:34:02', 127.80, 1, 'HB3000000', '375123456789'),
+  ('2021-09-24 12:34:02', 3.45, 2, 'HB3000001', '375123456780'),
+  ('2021-09-24 12:34:02', 127.80, 1, 'HB3000000', '375123456789'),
+  ('2021-09-24 12:34:02', 3.45, 2, 'HB3000001', '375123456780'),
+  ('2021-09-24 12:34:02', 127.80, 1, 'HB3000000', '375123456789'),
+  ('2021-09-24 12:34:02', 3.45, 2, 'HB3000001', '375123456780'),
+  ('2021-09-24 12:34:02', 127.80, 1, 'HB3000000', '375123456789'),
+  ('2021-09-24 12:34:02', 3.45, 2, 'HB3000001', '375123456780');
+SELECT
+  *
+FROM
+  `operations`;
