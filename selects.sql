73dc623bf328842d817f169dac7a5517022713ae
Grigory0 <grisha.konoplich0@gmail.com>
2021-11-21 21:07:42 +0300

    Lab 5

--- selects.sql
+++ selects.sql
@@ -10,3 +10,23 @@ SELECT fullName FROM `employees` WHERE position = 'Clean master';
 
 SELECT name, price, description, discount, (price-discount) AS currentPrice FROM `products` WHERE name = 'ukulele'
 ORDER BY currentPrice;
+
+
+
+-- Lab 5
+ SELECT name, COUNT(*) FROM `products` GROUP BY name;
+
+ 
+SELECT firstName FROM `clients`
+UNION
+SELECT fullName FROM `employees`;
+
+
+SELECT firstName FROM `clients`
+INTERSECT
+SELECT fullName FROM `employees`;
+
+
+SELECT firstName FROM `clients`
+EXCEPT
+SELECT fullName FROM `employees`;
